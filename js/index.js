bddca9ec4e61730fecb02062fa23ba4b87bd8325
afesta12 <andrewfesta11@yahoo.com>
2022-12-18 22:03:04 -0500

    Text slider

--- js/index.js
+++ js/index.js
@@ -5,6 +5,14 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 const slider = document.querySelector(".slider");
 let currentIndex = 0;
 
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const textCarousel = document.querySelector(".section__text");
+let direction;
+
+const arr = [0, 1, 2];
+
 buttons.forEach(button => {
 
     button.addEventListener("click", () => {
@@ -22,15 +30,16 @@ buttons.forEach(button => {
         if(newIndex < 0) newIndex = slides.children.length - 1;
         else if(newIndex >= slides.children.length) newIndex = 0;
 
+        // Text carousel
+        currentIndex += offset;
+        if(currentIndex > 2) currentIndex = 0;
+        if(currentIndex < 0) currentIndex = 2; 
+        const x = -33 * arr[currentIndex];
+        slider.style.transform = `translate(${x}%)`;
+
         // Add and remove active attribute to new active slide, and remove from old one
         slides.children[newIndex].dataset.active = true;
         delete activeSlide.dataset.active;
-
-        // Text slider
-        currentIndex = currentIndex + 1;
-        const translate = currentIndex * -33;
-        slider.style.transform = `translate(${translate}%)`;
-
     });
 
-})
+});
