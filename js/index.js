7cb659224369fe30fa0b749a9da66090b3e34d78
afesta12 <andrewfesta11@yahoo.com>
2022-12-19 11:58:10 -0500

    Fixed carousel issues

--- js/index.js
+++ js/index.js
@@ -5,12 +5,6 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 const slider = document.querySelector(".slider");
 let currentIndex = 0;
 
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-
-const textCarousel = document.querySelector(".section__text");
-let direction;
-
 const arr = [0, 1, 2];
 
 buttons.forEach(button => {
@@ -24,18 +18,24 @@ buttons.forEach(button => {
 
         const activeSlide = slides.querySelector("[data-active]");
 
+        
+
         let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 
         // Loops around to last image and vice versa
         if(newIndex < 0) newIndex = slides.children.length - 1;
         else if(newIndex >= slides.children.length) newIndex = 0;
 
-        // Text carousel
-        currentIndex += offset;
-        if(currentIndex > 2) currentIndex = 0;
-        if(currentIndex < 0) currentIndex = 2; 
-        const x = -33 * arr[currentIndex];
-        slider.style.transform = `translate(${x}%)`;
+        // Text slides
+        const textSlides = document.querySelector(".slider");
+        const active = textSlides.querySelector("[data-active]");
+        let cur = [...textSlides.children].indexOf(active) + offset;
+
+        if(cur < 0) cur = textSlides.children.length - 1;
+        else if(cur >= textSlides.children.length) cur = 0;
+
+        textSlides.children[cur].dataset.active = true;
+        delete active.dataset.active;
 
         // Add and remove active attribute to new active slide, and remove from old one
         slides.children[newIndex].dataset.active = true;
