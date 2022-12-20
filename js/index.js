f2d46e6c4db76dd8c685c4c7dc0175822d033d8a
afesta12 <andrewfesta11@yahoo.com>
2022-12-20 17:51:21 -0500

    Basic mobile nav

--- js/index.js
+++ js/index.js
@@ -7,6 +7,15 @@ let currentIndex = 0;
 
 const arr = [0, 1, 2];
 
+// Burger button
+const menuButton = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+
 buttons.forEach(button => {
 
     button.addEventListener("click", () => {
@@ -43,3 +52,27 @@ buttons.forEach(button => {
     });
 
 });
+
+menuButton.addEventListener("click", () => {
+
+    if(!showMenu) {
+
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open')
+        navItems.forEach(item => item.classList.add('open'));
+
+        showMenu = true;
+
+    } else {
+
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'))
+
+        showMenu = false;
+
+    }
+
+})
